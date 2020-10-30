c9536aceb5e47ef0e8339a8ed3bda090575ca7d5
2222-42 <stephen0617@me.com>
2020-10-30 11:12:39 +0900

    feat(chap3): finish 3.2.1

--- chapter03/variables/src/main.rs
+++ chapter03/variables/src/main.rs
@@ -6,8 +6,13 @@ fn main() {
 
     let spaces = "   ";
     let spaces = spaces.len();
-    println!("The length os spaces is {}", spaces)
+    println!("The length os spaces is {}", spaces);
 
     // let mut mspaces = "   ";
     // mspaces = mspaces.len();
+    let x = 2.0;
+    let y: f32 = 3.0;
+    println!("The value of x is {}", x);
+    println!("The value of y is {}", y);
+    // if calculate ( x + y ), the type of x is infferred to be f32.
 }
