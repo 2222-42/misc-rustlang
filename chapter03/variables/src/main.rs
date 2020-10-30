3754e953d64082e50c0763368ee4352023c05b44
2222-42 <stephen0617@me.com>
2020-10-30 10:56:59 +0900

    chore(chap3): finish 3.1

--- chapter03/variables/src/main.rs
+++ chapter03/variables/src/main.rs
@@ -1,6 +1,13 @@
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {}", x);
-    x = 6;
-    println!("The value of x is: {}", x);
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+    println!("The value of x is {}", x);
+
+    let spaces = "   ";
+    let spaces = spaces.len();
+    println!("The length os spaces is {}", spaces)
+
+    // let mut mspaces = "   ";
+    // mspaces = mspaces.len();
 }
