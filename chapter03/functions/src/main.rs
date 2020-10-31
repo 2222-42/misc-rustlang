b263525300eb8769e1807402d9c0241a7b9f9e90
2222-42 <stephen0617@me.com>
2020-10-31 18:00:54 +0900

    feat(chap3): finish 3.3.1

---
+++ chapter03/functions/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+
+    another_function(5, 6);
+}
+
+fn another_function(x: i32, y: i32) {
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+}
