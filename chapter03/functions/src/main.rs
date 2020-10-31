e95fa089dcb50cb8d1abb1dd01606970417e786e
2222-42 <stephen0617@me.com>
2020-10-31 21:06:57 +0900

    feat(chap3): finish section 3.3.2

--- chapter03/functions/src/main.rs
+++ chapter03/functions/src/main.rs
@@ -1,7 +1,14 @@
 fn main() {
     println!("Hello, world!");
 
-    another_function(5, 6);
+    // let x = (let y = 6);
+    let x = 5;
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    another_function(x, y);
 }
 
 fn another_function(x: i32, y: i32) {
