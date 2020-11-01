fcc827859fb3e17f1d814210923ed608419b6ea1
2222-42 <stephen0617@me.com>
2020-11-01 16:53:22 +0900

    feat(chap3): finish section 3.5.1.1

--- chapter03/branches/src/main.rs
+++ chapter03/branches/src/main.rs
@@ -1,10 +1,14 @@
 fn main() {
-    let number = 3;
+    let number = 6;
 
-    if number < 5 {
-        println!("condition was true");
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
     } else {
-        println!("condition was false");
+        println!("number is not divisible by 4, 3, 2");
     }
 
     // if number{
