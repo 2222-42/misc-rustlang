242a668752562741b9baf35bd465dc4227e3fb7e
2222-42 <stephen0617@me.com>
2020-11-01 16:49:52 +0900

    feat(chap3): finish 3.5.1.0

---
+++ chapter03/branches/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let number = 3;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    // if number{
+    //     println!("number was three");
+    // }
+    if number != 0 {
+        println!("number was something other than zero");
+    }
+}
