b144d1481fa7365e3fc014a6bf03b2d509bf5b5e
2222-42 <stephen0617@me.com>
2020-11-04 19:00:10 +0900

    chore(chapter4): move

--- chapter04/string_and_heap/src/main.rs
+++ chapter04/string_and_heap/src/main.rs
@@ -3,5 +3,14 @@ fn main() {
 
     s.push_str(", world!");
 
-    println!("{}", s)
+    println!("{}", s);
+
+    let x = 5;
+    let y = x;
+    println!("The value of x is {}, and one of y is {}", x, y);
+
+    let s1 = String::from("hello");
+    let s2 = s1;
+    // println!("The value of s1 is {}", s1);
+    println!("The value of s2 is {}", s2);
 }
