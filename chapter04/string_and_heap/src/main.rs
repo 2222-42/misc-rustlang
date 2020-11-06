68b4ed3db48af0d5a16ec92677dd0fc422657bbf
2222-42 <stephen0617@me.com>
2020-11-06 09:56:40 +0900

    feat(chap4): finish 4.1.7

--- chapter04/string_and_heap/src/main.rs
+++ chapter04/string_and_heap/src/main.rs
@@ -25,6 +25,16 @@ fn main() {
 
     makes_copy(x);
     println!("{}", x);
+
+    let s1 = give_ownership();
+    let s2 = String::from("hello");
+    let s3 = takes_and_gives_back(s2);
+    println!("The value of s1 is {}", s1);
+    // println!("The value of s2 is {}", s2);
+    println!("The value of s3 is {}", s3);
+
+    let (s4, len) = calculate_length(s3);
+    println!("The length of {} is: {}", s4, len);
 }
 
 fn takes_ownership(some_string: String) {
@@ -33,4 +43,19 @@ fn takes_ownership(some_string: String) {
 
 fn makes_copy(some_integer: i32) {
     println!("{}", some_integer);
+}
+
+fn give_ownership() -> String {
+    let some_string = String::from("hello");
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len();
+
+    (s, length)
 }
