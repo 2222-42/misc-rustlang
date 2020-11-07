14caca81f52bc4007cccff927b223c1ccf9d2151
2222-42 <stephen0617@me.com>
2020-11-07 10:27:08 +0900

    feat(chap4): finish 4.2.1

--- chapter04/refer_and_borrow/src/main.rs
+++ chapter04/refer_and_borrow/src/main.rs
@@ -3,13 +3,38 @@ fn main() {
     let len = calculate_length(&s1);
     println!("The length of '{}' is {}", s1, len);
 
-    change(&s1);
+    let mut s = String::from("Hello");
+    change(&mut s);
+    println!("{}", s);
+
+    let mut s = String::from("hello");
+    {
+        let r1 = &mut s;
+        // let r2 = &mut s;
+        change(r1);
+    }
+    let r2 = &mut s;
+    change(r2);
+    println!("{}", s);
+
+    mut_and_immut_borrow();
 }
 
 fn calculate_length(s: &String) -> usize {
     s.len()
 }
 
-fn change(some_string: &String) {
+fn change(some_string: &mut String) {
     some_string.push_str(", world")
+}
+
+fn mut_and_immut_borrow() {
+    let mut s = String::from("hello");
+    let r1 = &s;
+    let r2 = &s;
+    // let r3 = &mut s;
+    println!("{}", r1);
+    println!("{}", r2);
+    // change(r3);
+    // println!("{}", s);
 }
