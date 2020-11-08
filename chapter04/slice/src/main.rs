4625d530a72d3962b2808b51652e09798b9cffdf
2222-42 <stephen0617@me.com>
2020-11-08 20:06:03 +0900

    feat(chap4): finish 4.3.1.0

--- chapter04/slice/src/main.rs
+++ chapter04/slice/src/main.rs
@@ -1,22 +1,22 @@
 fn main() {
     let mut s = String::from("hello world");
-    let i = first_word(&s);
-    println!("The length of first word is :{}", i);
+    let word = first_word(&s);
+    println!("The first word is :{}", word);
     let world = &s[6..];
     println!("{}", world);
     println!("{}", &s[..]);
 
     s.clear();
-    println!("The length of first word is :{}", i);
+    // println!("The first word is :{}", word);
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word(s: &String) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[..i];
         }
     }
-    s.len()
+    &s[..]
 }
