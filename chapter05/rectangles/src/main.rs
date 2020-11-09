acf7d11674ab0aeaf7df61bc6dd07957135eb624
2222-42 <stephen0617@me.com>
2020-11-10 07:43:21 +0900

    feat(chap5): use Debug trait; finish 5.2

--- chapter05/rectangles/src/main.rs
+++ chapter05/rectangles/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -10,6 +11,8 @@ fn main() {
     };
 
     println!("The area of rectangle is {} square pixels.", area(&rect1));
+
+    println!("rect1 is: {:#?}", rect1);
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
