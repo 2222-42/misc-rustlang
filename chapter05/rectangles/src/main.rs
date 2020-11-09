2169a87e2ecf2580753b93bad9f0ecea78cebb89
2222-42 <stephen0617@me.com>
2020-11-10 07:33:30 +0900

    feat(chap5): use struct to refactor; finish 5.2.3

--- chapter05/rectangles/src/main.rs
+++ chapter05/rectangles/src/main.rs
@@ -1,18 +1,17 @@
-fn main() {
-    // let width1 = 30;
-    // let height1 = 50;
-    let rect1 = (30, 50);
-
-    println!(
-        "The area of rectangle is {} square pixels.",
-        // area(width1, height1)
-        area(rect1)
-    );
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
-// fn area(width: u32, height: u32) -> u32 {
-//     width * height
-// }
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("The area of rectangle is {} square pixels.", area(&rect1));
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
