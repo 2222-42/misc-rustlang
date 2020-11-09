334793b34b9012e271ae7ef8a7b63adb8586274e
2222-42 <stephen0617@me.com>
2020-11-10 07:56:21 +0900

    feat(chap5): use impl; finish 5.3.1

--- chapter05/rectangles/src/main.rs
+++ chapter05/rectangles/src/main.rs
@@ -4,17 +4,19 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
 
-    println!("The area of rectangle is {} square pixels.", area(&rect1));
+    println!("The area of rectangle is {} square pixels.", rect1.area());
 
     println!("rect1 is: {:#?}", rect1);
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
 }
