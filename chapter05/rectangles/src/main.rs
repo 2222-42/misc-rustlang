d8d1c4fd9d28240fdf1b328493dbb0daa57e380b
2222-42 <stephen0617@me.com>
2020-11-11 12:08:12 +0900

    feat(chap5): finish chapter 5

--- chapter05/rectangles/src/main.rs
+++ chapter05/rectangles/src/main.rs
@@ -11,6 +11,12 @@ impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
@@ -34,4 +40,7 @@ fn main() {
 
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect3));
+
+    let rect4 = Rectangle::square(5);
+    println!("rect4 is {:?}", rect4);
 }
