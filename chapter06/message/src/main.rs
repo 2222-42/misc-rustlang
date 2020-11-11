6b90d63eac98f6b29dd62618e7df2037e0a7df51
2222-42 <stephen0617@me.com>
2020-11-11 13:51:29 +0900

    chore(chap6): define impl to Message

---
+++ chapter06/message/src/main.rs
@@ -0,0 +1,24 @@
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {}
+}
+
+// struct QuitMessage;
+// struct MoveMessage {
+//     x: i32,
+//     y: i32,
+// }
+// struct WriteMessage(String);
+// struct ChangeColorMessage(i32, i32, i32);
+
+fn main() {
+    let m = Message::Write(String::from("hello"));
+    m.call();
+    println!("Hello, world!");
+}
