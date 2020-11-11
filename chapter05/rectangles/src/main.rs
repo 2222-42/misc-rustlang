c3a8004021cbddfd63abf4fe6c9f6d76e87e1290
2222-42 <stephen0617@me.com>
2020-11-11 11:53:53 +0900

    feat(chap5): finish 5.3.3

--- chapter05/rectangles/src/main.rs
+++ chapter05/rectangles/src/main.rs
@@ -8,6 +8,9 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
 fn main() {
@@ -19,4 +22,16 @@ fn main() {
     println!("The area of rectangle is {} square pixels.", rect1.area());
 
     println!("rect1 is: {:#?}", rect1);
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect3));
 }
