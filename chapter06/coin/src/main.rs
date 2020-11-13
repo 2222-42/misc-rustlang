a99ecffae2d0d560631eb98fbeee445e36a824bc
2222-42 <stephen0617@me.com>
2020-11-13 09:45:47 +0900

    feat(chap6): finish 6.2.1

--- chapter06/coin/src/main.rs
+++ chapter06/coin/src/main.rs
@@ -1,8 +1,62 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    Arizona,
+    Arkansas,
+    California,
+    Colorado,
+    Connecticut,
+    Delaware,
+    Florida,
+    Georgia,
+    Hawaii,
+    Idaho,
+    Illinois,
+    Indiana,
+    Iowa,
+    Kansas,
+    Kentucky,
+    Louisiana,
+    Maine,
+    Maryland,
+    Massachusetts,
+    Michigan,
+    Minnesota,
+    Mississippi,
+    Missouri,
+    Montana,
+    Nebraska,
+    Nevada,
+    NewHampshire,
+    NewJersey,
+    NewMexico,
+    NewYork,
+    NorthCarolina,
+    NorthDakota,
+    Ohio,
+    Oklahoma,
+    Oregon,
+    Pennsylvania,
+    RhodeIsland,
+    SouthCarolina,
+    SouthDakota,
+    Tennessee,
+    Texas,
+    Utah,
+    Vermont,
+    Virginia,
+    Washington,
+    WestVirginia,
+    Wisconsin,
+    Wyoming,
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u32 {
@@ -13,12 +67,17 @@ fn value_in_cents(coin: Coin) -> u32 {
         }
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from: {:?}!", state);
+            25
+        }
     }
 }
 fn main() {
     let coin = Coin::Nickel;
     println!("{}", value_in_cents(coin));
     let coin = Coin::Penny;
+    println!("{}", value_in_cents(coin));
+    let coin = Coin::Quarter(UsState::Alaska);
     println!("{}", value_in_cents(coin));
 }
