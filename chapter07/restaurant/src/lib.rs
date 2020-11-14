76065d8de4338a824dc289efabb0f00d34bd8d0e
2222-42 <stephen0617@me.com>
2020-11-14 15:11:32 +0900

    chore(chapter7): add fn eat_at_restaurant()

--- chapter07/restaurant/src/lib.rs
+++ chapter07/restaurant/src/lib.rs
@@ -14,6 +14,14 @@ mod front_of_house {
     }
 }
 
+pub fn eat_at_restaurant() {
+    // absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // relative path
+    front_of_house::hosting::add_to_waitlist();
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
