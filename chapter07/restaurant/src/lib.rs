2f7d930e442c3a9c24e022dbe40f10f688b71f6f
2222-42 <stephen0617@me.com>
2020-11-16 11:08:33 +0900

    chore(chap7): use keyword `use`

--- chapter07/restaurant/src/lib.rs
+++ chapter07/restaurant/src/lib.rs
@@ -42,6 +42,9 @@ mod back_of_house {
     }
 }
 
+// use crate::front_of_house::hosting;
+use self::front_of_house::hosting;
+
 pub fn eat_at_restaurant() {
     let mut meal = back_of_house::Breakfast::summer("Rye");
     meal.toast = String::from("Wheat");
@@ -54,10 +57,11 @@ pub fn eat_at_restaurant() {
     let order2 = back_of_house::Appetizer::Salad;
 
     // absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
-
+    // crate::front_of_house::hosting::add_to_waitlist();
     // relative path
-    front_of_house::hosting::add_to_waitlist();
+    // front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 }
 
 #[cfg(test)]
