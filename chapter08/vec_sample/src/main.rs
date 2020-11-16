e004b34c469292aacfe9c977645243662b0224c4
2222-42 <stephen0617@me.com>
2020-11-17 07:52:30 +0900

    feat(chap8): use push to add data into vector

--- chapter08/vec_sample/src/main.rs
+++ chapter08/vec_sample/src/main.rs
@@ -2,5 +2,11 @@ fn main() {
     let v: Vec<i32> = Vec::new();
     let v2 = vec![1, 2, 3];
 
+    let mut mv = Vec::new();
+    mv.push(5);
+    mv.push(6);
+    mv.push(7);
+    mv.push(5);
+
     println!("Hello, world!");
 }
