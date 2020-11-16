e41aeca1f1400bde632f3f6b6df7966f66b42022
2222-42 <stephen0617@me.com>
2020-11-16 10:21:40 +0900

    chore(*): use super

--- chapter07/restaurant/src/lib.rs
+++ chapter07/restaurant/src/lib.rs
@@ -1,14 +1,21 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
         fn seat_at_table() {}
     }
 
-    mod serving {
+    pub mod serving {
         fn take_order() {}
 
         fn serve_order() {}
+        mod back_of_house {
+            fn fix_incorrect_order() {
+                cook_order();
+                super::serve_order();
+            }
+            fn cook_order() {}
+        }
 
         fn take_payment() {}
     }
