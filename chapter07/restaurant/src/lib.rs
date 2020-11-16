b7888db0b8c07b16f399ccc63269b5d5245fb05e
2222-42 <stephen0617@me.com>
2020-11-16 10:43:13 +0900

    chore(chap7): add impl to construct pub struct

--- chapter07/restaurant/src/lib.rs
+++ chapter07/restaurant/src/lib.rs
@@ -21,7 +21,30 @@ mod front_of_house {
     }
 }
 
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peachess"),
+            }
+        }
+    }
+}
+
 pub fn eat_at_restaurant() {
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+
+    println!("I'd like {} toast please", meal.toast);
+    // field `seasonal_fruit` of struct `back_of_house::Breakfast` is private
+    // meal.seasonal_fruit = String::from("blueberries");
+
     // absolute path
     crate::front_of_house::hosting::add_to_waitlist();
 
