0ce103911d1aa646db5ff67df34538aed436c69d
2222-42 <stephen0617@me.com>
2020-11-19 08:04:12 +0900

    feat(chap8): generate new HashMap

---
+++ chapter08/hash_map_sample/src/main.rs
@@ -0,0 +1,12 @@
+use std::collections::HashMap;
+fn main() {
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+
+    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+}
