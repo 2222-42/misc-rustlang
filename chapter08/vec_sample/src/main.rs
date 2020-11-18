3ba6217725d680881b074b9cc904b1dfadb288b6
2222-42 <stephen0617@me.com>
2020-11-18 10:38:32 +0900

    chore(chap8): check borrow checker of vector

--- chapter08/vec_sample/src/main.rs
+++ chapter08/vec_sample/src/main.rs
@@ -1,7 +1,12 @@
 fn main() {
     let v: Vec<i32> = Vec::new();
     {
-        let v2 = vec![1, 2, 3];
+        let v2 = vec![1, 2, 3, 4, 5];
+        let third = &v2[2];
+        println!("The third element is {}", third);
+        let third = v2.get(2);
+        // let does_not_exist = &v2[100];
+        let does_not_exist = &v2.get(100);
     }
 
     let mut mv = Vec::new();
@@ -9,6 +14,8 @@ fn main() {
     mv.push(6);
     mv.push(7);
     mv.push(5);
+    let first = &mv[0];
 
-    println!("Hello, world!");
+    // mv.push(6);
+    println!("The third element is {}", first);
 }
