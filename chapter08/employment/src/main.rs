9beffdd8f77e1b6f22ed65d7a68061e45d0f4254
2222-42 <stephen0617@me.com>
2020-11-22 14:33:38 +0900

    chore(chap8/employment): trim

--- chapter08/employment/src/main.rs
+++ chapter08/employment/src/main.rs
@@ -30,20 +30,34 @@ impl Employee {
 fn main() {
     let mut members: Vec<Employee> = Vec::new();
     loop {
-        println!("Please input new employment.");
+        println!("Please input.");
         let mut order = String::new();
         io::stdin()
             .read_line(&mut order)
             .expect("Failed to read line");
-        let person = parse_input(order);
-        person.enter_member(&mut members);
-        println!("{:?}", members)
+        let order: String = match order.trim().parse() {
+            Ok(string) => string,
+            Err(_) => continue,
+        };
+        parse_input(order, &mut members);
     }
 }
 
-fn parse_input(order: String) -> Employee {
+fn parse_input(order: String, members: &mut Vec<Employee>) {
     let order_map: Vec<&str> = order.split(" ").collect();
-    let name = order_map[1].to_string();
-    let department = order_map[3].to_string();
-    Employee { name, department }
+    let command = order_map[0];
+    match command {
+        "Add" => {
+            let name = order_map[1].to_string();
+            let department = order_map[3].to_string();
+            add_member(name, department, members);
+        }
+        _ => (),
+    }
+}
+
+fn add_member(name: String, department: String, members: &mut Vec<Employee>) {
+    let person = Employee { name, department };
+    person.enter_member(members);
+    println!("{:?}", members)
 }
