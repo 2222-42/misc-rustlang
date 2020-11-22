ddfc0c16f44c9298c0b887adbb2a4b917bf9db46
2222-42 <stephen0617@me.com>
2020-11-22 11:24:15 +0900

    chore(chap8): modify match

--- chapter08/string_exercise/src/main.rs
+++ chapter08/string_exercise/src/main.rs
@@ -12,11 +12,7 @@ fn pig_latin(str: &str) -> String {
     let s = &str[0..1];
     let rest = &str[1..];
     match s {
-        "a" => format!("{}-hay", str),
-        "i" => format!("{}-hay", str),
-        "u" => format!("{}-hay", str),
-        "e" => format!("{}-hay", str),
-        "o" => format!("{}-hay", str),
+        "a" | "i" | "u" | "e" | "o" => format!("{}-hay", str),
         _ => format!("{}-{}ay", rest, s),
     }
 }
