bddf653cff539bf1baf69863aaa3a4efe8bb41f5
2222-42 <stephen0617@me.com>
2020-11-22 11:37:45 +0900

    chore(chap*): use chars

--- chapter08/string_exercise/src/main.rs
+++ chapter08/string_exercise/src/main.rs
@@ -2,9 +2,13 @@ fn main() {
     let text1 = "first";
     let result = pig_latin(text1);
     println!("{}", result);
+    let result = pig_latin_use_char(text1);
+    println!("{}", result);
 
     let text2 = "apple";
     let result = pig_latin(text2);
+    println!("{}", result);
+    let result = pig_latin_use_char(text2);
     println!("{}", result);
 }
 
@@ -14,5 +18,21 @@ fn pig_latin(str: &str) -> String {
     match s {
         "a" | "i" | "u" | "e" | "o" => format!("{}-hay", str),
         _ => format!("{}-{}ay", rest, s),
+    }
+}
+
+fn pig_latin_use_char(str: &str) -> String {
+    let mut chars = str.chars();
+    let s = chars.nth(0);
+    match s {
+        Some('a') | Some('i') | Some('u') | Some('e') | Some('o') => format!("{}-hay", str),
+        Some(v) => {
+            let mut rest = String::new();
+            for elem in chars {
+                rest = format!("{}{}", rest, elem)
+            }
+            format!("{}-{}ay", rest, v.to_string())
+        }
+        None => format!(""),
     }
 }
