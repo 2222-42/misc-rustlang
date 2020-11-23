c823421e49194d6ef526d3dd89b9277ffdea72da
2222-42 <stephen0617@me.com>
2020-11-24 07:18:05 +0900

    chore(*): use match to handle Result

--- chapter09/panic_sample/src/main.rs
+++ chapter09/panic_sample/src/main.rs
@@ -1,5 +1,16 @@
+use std::fs::File;
 fn main() {
     // panic!("crash and burn");
-    let v = vec![1, 2, 3];
-    v[99];
+    // let v = vec![1, 2, 3];
+    // v[99];
+
+    let f = File::open("hello.txt");
+    /*= note: expected type `u32`
+    found enum `std::result::Result<File, std::io::Error>`*/
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => {
+            panic!("There was a problem opening the file: {:?}", error)
+        }
+    };
 }
