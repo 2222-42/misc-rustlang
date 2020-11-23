71974881bef64f6129aaa524b8b7683eb0e140f4
2222-42 <stephen0617@me.com>
2020-11-24 07:50:22 +0900

    chore(chap9): use unwrap and expect

--- chapter09/panic_sample/src/main.rs
+++ chapter09/panic_sample/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs::File;
+use std::{fs::File, io::ErrorKind};
 fn main() {
     // panic!("crash and burn");
     // let v = vec![1, 2, 3];
@@ -9,8 +9,18 @@ fn main() {
     found enum `std::result::Result<File, std::io::Error>`*/
     let f = match f {
         Ok(file) => file,
+        Err(ref error) if error.kind() == ErrorKind::NotFound => match File::create("hello.txt") {
+            Ok(fc) => fc,
+            Err(e) => {
+                panic!("Tried to create file but there was a problem: {:?}", e)
+            }
+        },
         Err(error) => {
             panic!("There was a problem opening the file: {:?}", error)
         }
     };
+
+    let f = File::open("hello.txt").unwrap();
+
+    let f = File::open("hello.txt").expect("Failed to open hello.txt");
 }
