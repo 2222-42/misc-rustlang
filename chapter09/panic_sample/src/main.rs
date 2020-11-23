87f5fdf14f080e6eba6c9f9f3d421fb02b22fc0c
2222-42 <stephen0617@me.com>
2020-11-24 08:01:38 +0900

    chore(chap9): use error_transfer

--- chapter09/panic_sample/src/main.rs
+++ chapter09/panic_sample/src/main.rs
@@ -1,4 +1,24 @@
-use std::{fs::File, io::ErrorKind};
+use std::{
+    fs::File,
+    io::{self, ErrorKind, Read},
+};
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => {
+            return Err(e);
+        }
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
 fn main() {
     // panic!("crash and burn");
     // let v = vec![1, 2, 3];
@@ -19,8 +39,6 @@ fn main() {
             panic!("There was a problem opening the file: {:?}", error)
         }
     };
-
-    let f = File::open("hello.txt").unwrap();
-
-    let f = File::open("hello.txt").expect("Failed to open hello.txt");
+    // let f = File::open("hello.txt").unwrap();
+    // let f = File::open("hello.txt").expect("Failed to open hello.txt");
 }
