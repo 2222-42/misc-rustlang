ced1ed72769c38e094137bbe1f00e21ef88945d5
2222-42 <stephen0617@me.com>
2020-11-25 07:17:08 +0900

    chose(*): use read_username_from_file

--- chapter09/panic_sample/src/main.rs
+++ chapter09/panic_sample/src/main.rs
@@ -28,18 +28,23 @@ fn main() {
     let f = File::open("hello.txt");
     /*= note: expected type `u32`
     found enum `std::result::Result<File, std::io::Error>`*/
-    let _f = match f {
-        Ok(file) => file,
-        Err(ref error) if error.kind() == ErrorKind::NotFound => match File::create("hello.txt") {
-            Ok(fc) => fc,
-            Err(e) => {
-                panic!("Tried to create file but there was a problem: {:?}", e)
-            }
-        },
-        Err(error) => {
-            panic!("There was a problem opening the file: {:?}", error)
-        }
-    };
+    // let _f = match f {
+    //     Ok(file) => file,
+    //     Err(ref error) if error.kind() == ErrorKind::NotFound => match File::create("hello.txt") {
+    //         Ok(fc) => fc,
+    //         Err(e) => {
+    //             panic!("Tried to create file but there was a problem: {:?}", e)
+    //         }
+    //     },
+    //     Err(error) => {
+    //         panic!("There was a problem opening the file: {:?}", error)
+    //     }
+    // };
     // let f = File::open("hello.txt").unwrap();
     // let f = File::open("hello.txt").expect("Failed to open hello.txt");
+    let result = read_username_from_file();
+    match result {
+        Ok(v) => println!("use name is {}",v),
+        Err(e) => panic!("There was a problem opening the file: {:?}", e)
+    }
 }
