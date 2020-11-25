71bae99c061c50b5927a91e853a9c834641bc5b7
2222-42 <stephen0617@me.com>
2020-11-25 11:19:56 +0900

    feat(chap9): use instance to check

--- chapter02/gussing_game/src/main.rs
+++ chapter02/gussing_game/src/main.rs
@@ -5,6 +5,25 @@ use std::{cmp::Ordering, io};
 // use std::cmp::Ordering;
 // use std::io;
 
+pub struct Guess {
+    value: u32,
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("The secret number will be between 1 and 100, got: {}", value);
+        } 
+        Guess {
+            value,
+        }
+    }
+
+    pub fn value(&self) -> u32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1, 101);
@@ -15,14 +34,10 @@ fn main() {
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-        let guess: i32 = match guess.trim().parse() {
-            Ok(num) => num,
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => Guess::new(num).value(),
             Err(_) => continue,
         };
-        if guess < 1 || guess > 100 {
-            println!("The secret number will be between 1 and 100.");
-            continue;
-        } 
         println!("You guessed: {}", guess);
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
