4228a4eec604fcfc9e6aea113be776d5844999e2
2222-42 <stephen0617@me.com>
2020-11-27 08:27:12 +0900

    chore(chap10): add method

--- chapter10/generic_struct_and_enum/src/main.rs
+++ chapter10/generic_struct_and_enum/src/main.rs
@@ -1,6 +1,16 @@
+#[derive(Debug)]
 struct Point<T, U> {
     x: T,
     y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
 }
 
 struct SimplePoint<T> {
@@ -24,6 +34,9 @@ fn main() {
     let both_integer = Point { x: 5, y: 10 };
     let both_float = Point { x: 1.0, y: 4.0 };
     let integer_and_float = Point { x: 5, y: 4.0 };
+
+    let new_pointer = both_integer.mixup(both_float);
+    println!("{:?}", new_pointer);
 
     let point = SimplePoint { x: 5, y: 10 };
     println!("p.x = {}", point.x);
