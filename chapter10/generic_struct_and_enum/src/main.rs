c5f33507b000ef68d224e8eb14b2321947f19ef5
2222-42 <stephen0617@me.com>
2020-11-27 08:10:54 +0900

    chore(chap10): add method

--- chapter10/generic_struct_and_enum/src/main.rs
+++ chapter10/generic_struct_and_enum/src/main.rs
@@ -3,8 +3,36 @@ struct Point<T, U> {
     y: U,
 }
 
+struct SimplePoint<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> SimplePoint<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl SimplePoint<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
 fn main() {
     let both_integer = Point { x: 5, y: 10 };
     let both_float = Point { x: 1.0, y: 4.0 };
     let integer_and_float = Point { x: 5, y: 4.0 };
+
+    let point = SimplePoint { x: 5, y: 10 };
+    println!("p.x = {}", point.x);
+
+    let both_float = SimplePoint { x: 1.0, y: 4.0 };
+    println!(
+        "the distance from origin is {}",
+        both_float.distance_from_origin()
+    );
+
+    // point.distance_from_origin();
 }
