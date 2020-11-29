a69f148a01adc79b8fa0d9a37380b19903045b6f
2222-42 <stephen0617@me.com>
2020-11-29 14:05:55 +0900

    fix(chap10): fix largest

--- chapter10/generic_function_sample/src/main.rs
+++ chapter10/generic_function_sample/src/main.rs
@@ -22,7 +22,10 @@ fn largest_char(list: &[char]) -> char {
     largest
 }
 
-fn largest<T>(list: &[T]) -> T {
+fn largest<T>(list: &[T]) -> T
+where
+    T: PartialOrd + Copy,
+{
     let mut largest = list[0];
 
     for &number in list.iter() {
@@ -35,13 +38,13 @@ fn largest<T>(list: &[T]) -> T {
 }
 
 fn main() {
-    let number_list = vec![24, 59, 25, 100,65];
+    let number_list = vec![24, 59, 25, 100, 65];
 
     let result = largest(&number_list);
     println!("The largest number is {}", result);
     assert_eq!(result, 100);
 
-    let char_list = vec!['y', 'm','a', 'q'];
+    let char_list = vec!['y', 'm', 'a', 'q'];
 
     let result = largest(&char_list);
     println!("The largest char is {}", result);
