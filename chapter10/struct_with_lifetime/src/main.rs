db0401d6429d9167207eafc7b47ccf4937b407c0
2222-42 <stephen0617@me.com>
2020-12-02 14:52:21 +0900

    feat(chap10): make method definition without lifetime annotation

--- chapter10/struct_with_lifetime/src/main.rs
+++ chapter10/struct_with_lifetime/src/main.rs
@@ -2,8 +2,23 @@ struct ImportantExcerpt<'a> {
     part: &'a str,
 }
 
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+
+    fn announce_and_return_part(&self, annoucement: &str) -> &str {
+        println!("Attention please: {}", annoucement);
+        self.part
+    }
+}
+
 fn main() {
     let novel = String::from("Call me Ishmael. Some years ago...");
     let first_sentne = novel.split('.').next().expect("Could not find a '.'");
     let i = ImportantExcerpt { part: first_sentne };
+
+    println!("level: {}", i.level());
+    let s = i.announce_and_return_part("");
+    println!("{}", s);
 }
