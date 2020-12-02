2bde5d0b253ec270d0e87cc8dd3ecfa74dc4504a
2222-42 <stephen0617@me.com>
2020-12-02 13:47:28 +0900

    feat(chap10): use life annotation in function

--- chapter10/generic_function_lifetime/src/main.rs
+++ chapter10/generic_function_lifetime/src/main.rs
@@ -1,4 +1,4 @@
-fn longest(x: &str, y: &str) -> &str {
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
     } else {
@@ -8,7 +8,11 @@ fn longest(x: &str, y: &str) -> &str {
 
 fn main() {
     let string1 = String::from("abcd");
-    let string2 = "xyz";
-    let result = longest(string1.as_str(), string2);
-    println!("The longest string is {}", result);
+    let result;
+    {
+        let string2 = String::from("xyz");
+        result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
+    }
+    // println!("The longest string is {}", result);
 }
