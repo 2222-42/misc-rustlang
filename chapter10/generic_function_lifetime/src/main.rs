c12dc71b904057644237ce6ba5725736ec501c62
2222-42 <stephen0617@me.com>
2020-12-04 19:47:31 +0900

    feat(chap10): finish chap10

--- chapter10/generic_function_lifetime/src/main.rs
+++ chapter10/generic_function_lifetime/src/main.rs
@@ -1,4 +1,18 @@
+use std::fmt::Display;
+
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn longest_with_an_an_anouncement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
     if x.len() > y.len() {
         x
     } else {
@@ -13,6 +27,9 @@ fn main() {
         let string2 = String::from("xyz");
         result = longest(string1.as_str(), string2.as_str());
         println!("The longest string is {}", result);
+        let result2 =
+            longest_with_an_an_anouncement(string1.as_str(), string2.as_str(), "Hooooh!!");
+        println!("The longest string is {}", result2);
     }
     // println!("The longest string is {}", result);
 }
