c2048e88cfe75b0997e6457c321959bf595cf656
2222-42 <stephen0617@me.com>
2020-12-05 14:51:38 +0900

    feat(chap11): use assert_eq! and assert_ne!

--- chapter11/adder/src/lib.rs
+++ chapter11/adder/src/lib.rs
@@ -1,8 +1,19 @@
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn exploration() {
-        assert_eq!(2 + 2, 4);
+        assert_eq!(add_two(2), 4);
+    }
+
+    #[test]
+    fn test_ne_case() {
+        assert_ne!(5, add_two(2))
     }
 
     // #[test]
