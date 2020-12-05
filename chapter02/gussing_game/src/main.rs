42718156d51711a30eaa57947706ec51dc9620e1
2222-42 <stephen0617@me.com>
2020-12-06 07:41:07 +0900

    chore(chap11): use should_panic with expected

--- chapter02/gussing_game/src/main.rs
+++ chapter02/gussing_game/src/main.rs
@@ -13,7 +13,12 @@ impl Guess {
     pub fn new(value: u32) -> Guess {
         if value < 1 {
             panic!(
-                "The secret number will be between 1 and 100, got: {}",
+                "Guess value mus be greater than or equal to 1, got: {}",
+                value
+            );
+        } else if value > 100 {
+            panic!(
+                "Guess value mus be less than or equal to 100, got: {}",
                 value
             );
         }
@@ -30,7 +35,7 @@ mod tests {
     use super::*;
 
     #[test]
-    #[should_panic]
+    #[should_panic(expected = "Guess value mus be less than or equal to 100")]
     fn test_greater_than_100() {
         Guess::new(200);
     }
