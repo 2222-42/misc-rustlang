63a30cf5539346901eaf7cb7d844eb68821840e2
2222-42 <stephen0617@me.com>
2020-12-06 08:36:05 +0900

    chore(chap11): check the behaviour of capture

--- chapter11/adder/src/lib.rs
+++ chapter11/adder/src/lib.rs
@@ -2,9 +2,25 @@ pub fn add_two(a: i32) -> i32 {
     a + 2
 }
 
+pub fn prints_and_retunrs_10(a: i32) -> i32 {
+    println!("I got the value {}", a);
+    10
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_will_pass() {
+        let value = prints_and_retunrs_10(4);
+        assert_eq!(10, value)
+    }
+    // #[test]
+    // fn test_will_fail() {
+    //     let value = prints_and_retunrs_10(5);
+    //     assert_eq!(5, value)
+    // }
     #[test]
     fn it_works() -> Result<(), String> {
         if 2 + 2 == 4 {
