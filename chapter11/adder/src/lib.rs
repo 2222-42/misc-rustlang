f4210dc90724b1f98e66e2f8724dd905d199f64e
2222-42 <stephen0617@me.com>
2020-12-06 08:02:46 +0900

    chore(chap11): use result to test

--- chapter11/adder/src/lib.rs
+++ chapter11/adder/src/lib.rs
@@ -5,6 +5,14 @@ pub fn add_two(a: i32) -> i32 {
 #[cfg(test)]
 mod tests {
     use super::*;
+    #[test]
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
 
     #[test]
     fn exploration() {
