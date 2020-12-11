c129b7e14910db81a14e98d8ffaaed406ab0facb
2222-42 <stephen0617@me.com>
2020-12-11 10:32:21 +0900

    feat(chap12): access file and store locally

--- chapter12/minigrep/src/main.rs
+++ chapter12/minigrep/src/main.rs
@@ -1,6 +1,19 @@
-use std::env;
+use std::io::prelude::*;
+use std::{env, fs::File};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+
+    let query = &args[1];
+    let filename = &args[2];
+    println!("Searching for {:?}", query);
+    println!("In file {:?}", filename);
+
+    let mut f = File::open(filename).expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
