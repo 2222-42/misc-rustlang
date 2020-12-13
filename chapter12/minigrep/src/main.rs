0f151b1b9176a9e1cb39459ae6eac6c3e3d0583b
2222-42 <stephen0617@me.com>
2020-12-13 10:27:22 +0900

    feat(chap12): abstract run and handle Erorr

--- chapter12/minigrep/src/main.rs
+++ chapter12/minigrep/src/main.rs
@@ -1,20 +1,31 @@
-use std::io::prelude::*;
-use std::{env, fs::File};
+use std::{env, error::Error, fs::File};
+use std::{io::prelude::*, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing argments: {}", err);
+        process::exit(1);
+    });
     println!("Searching for {:?}", config.query);
     println!("In file {:?}", config.filename);
 
-    let mut f = File::open(config.filename).expect("file not found");
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let mut f = File::open(config.filename)?;
 
     let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
+    f.read_to_string(&mut contents)?;
 
     println!("With text:\n{}", contents);
+
+    Ok(())
 }
 
 struct Config {
@@ -23,12 +34,12 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("not enough arguments");
+            return Err("not enough arguments");
         }
         let query = args[1].clone();
         let filename = args[2].clone();
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
