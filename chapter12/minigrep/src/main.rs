6a7dc9a8e4b2e9da6d211d132d3aaf74293e2da2
2222-42 <stephen0617@me.com>
2020-12-13 09:33:56 +0900

    chore(chap12): use imple new

--- chapter12/minigrep/src/main.rs
+++ chapter12/minigrep/src/main.rs
@@ -4,7 +4,7 @@ use std::{env, fs::File};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
     println!("Searching for {:?}", config.query);
     println!("In file {:?}", config.filename);
 
@@ -22,8 +22,10 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-    Config { query, filename }
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config { query, filename }
+    }
 }
