90fc30345a66f39b358e73ce556b7013e0d5456e
2222-42 <stephen0617@me.com>
2020-12-18 11:03:53 +0900

    chore(chap13): add memo

--- chapter13/training_plan/src/main.rs
+++ chapter13/training_plan/src/main.rs
@@ -14,13 +14,16 @@ fn simulated_expensive_calculation(intensity: u32) -> u32 {
 
 fn generate_workout(intensity: u32, random_number: u32) {
     let expensive_closure = |num| {
+        // let expensive_closure = |num: u32| -> u32 {
         println!("calculating slowly...");
         thread::sleep(Duration::from_secs(2));
         num
     };
+
     if intensity < 25 {
         println!("Today, do {} pushups!", expensive_closure(intensity));
-        println!("Next, do {} situps!", expensive_closure(intensity))
+        println!("Next, do {} situps!", expensive_closure(intensity));
+    // expensive_closure(String::from("test"))
     } else {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated!");
