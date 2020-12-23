f1621c4b18eb5eae94fb8b0089816b90e17d459e
2222-42 <stephen0617@me.com>
2020-12-24 06:59:01 +0900

    chore(chap13): test about sum

--- chapter13/sample_iterator/src/lib.rs
+++ chapter13/sample_iterator/src/lib.rs
@@ -11,4 +11,15 @@ mod test_super {
         assert_eq!(v1_iter.next(), Some(&3));
         assert_eq!(v1_iter.next(), None);
     }
+    #[test]
+    fn iterator_sum() {
+        let v1 = vec![1, 2, 3];
+        let v1_iter = v1.iter();
+
+        let total: i32 = v1_iter.sum();
+        assert_eq!(6, total);
+
+        // let total2: i32 = v1_iter.sum();
+        // assert_eq!(6, total);
+    }
 }
