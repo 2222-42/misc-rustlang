ec130749de792bb7897df4aceef9c19a1a696ac1
2222-42 <stephen0617@me.com>
2020-12-24 06:57:44 +0900

    chore(chap13): test about next

---
+++ chapter13/sample_iterator/src/lib.rs
@@ -0,0 +1,14 @@
+#[cfg(test)]
+mod test_super {
+    use super::*;
+
+    #[test]
+    fn iter_demonstration() {
+        let v1 = vec![1, 2, 3];
+        let mut v1_iter = v1.iter();
+        assert_eq!(v1_iter.next(), Some(&1));
+        assert_eq!(v1_iter.next(), Some(&2));
+        assert_eq!(v1_iter.next(), Some(&3));
+        assert_eq!(v1_iter.next(), None);
+    }
+}
