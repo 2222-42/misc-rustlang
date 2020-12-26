35d4ac3582145fe7401c7195f3d9ef858ccc90a9
2222-42 <stephen0617@me.com>
2020-12-26 15:14:41 +0900

    chore(chap14): first commit

---
+++ chapter14/my_crate/src/lib.rs
@@ -0,0 +1,20 @@
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let five = 5;
+/// assert_eq!(6, my_crate::add_one(5))
+/// ```
+///  
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
