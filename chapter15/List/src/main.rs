8b0f9d993ca9cb1efe033cd8988a10c58b39fd1a
2222-42 <stephen0617@me.com>
2021-01-03 06:57:00 +0900

    chroe(chap15): use std::rc::Rc

--- chapter15/List/src/main.rs
+++ chapter15/List/src/main.rs
@@ -1,9 +1,14 @@
 enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
 }
 
+use List::{Cons, Nil};
+use std::rc::Rc;
+
 fn main() {
-    use List::{Cons, Nil};
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    let a = Rc::new(Cons(1, Rc::new(Cons(2, Rc::new(Cons(3, Rc::new(Nil)))))));
+    let b = Cons(5, Rc::clone(&a));
+    let c = Cons(10, Rc::clone(&a));
 }
