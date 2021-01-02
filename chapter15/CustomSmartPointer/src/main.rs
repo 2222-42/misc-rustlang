8f92ac15da082631a668537a7fe055f90e9b1fbb
2222-42 <stephen0617@me.com>
2021-01-03 06:18:27 +0900

    chore(chap15): use std::mem::drop

--- chapter15/CustomSmartPointer/src/main.rs
+++ chapter15/CustomSmartPointer/src/main.rs
@@ -11,7 +11,7 @@ impl Drop for CustomSmartPointer {
 fn main() {
     let c = CustomSmartPointer{data: String::from("my stuff")};
     let d = CustomSmartPointer{data: String::from("other stuff")};
-
     println!("CustomSmartPointers created");
+    drop(c);
 }
 
