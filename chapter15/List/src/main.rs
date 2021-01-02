d7585685b8f305214a481bf6072596e1bb433369
2222-42 <stephen0617@me.com>
2021-01-03 08:15:00 +0900

    chore(chap15): strong_count

--- chapter15/List/src/main.rs
+++ chapter15/List/src/main.rs
@@ -9,6 +9,13 @@ use std::rc::Rc;
 fn main() {
 
     let a = Rc::new(Cons(1, Rc::new(Cons(2, Rc::new(Cons(3, Rc::new(Nil)))))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
     let b = Cons(5, Rc::clone(&a));
-    let c = Cons(10, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(10, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
