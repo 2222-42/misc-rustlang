b99541f88646ad9924ddfcb322b43287fd4fd97a
2222-42 <stephen0617@me.com>
2021-01-06 18:30:46 +0900

    feat(chap15): add parent, using Weak

--- chapter15/my_tree/src/main.rs
+++ chapter15/my_tree/src/main.rs
@@ -1,19 +1,29 @@
-use std::rc::Rc;
 use std::cell::RefCell;
+use std::rc::{Rc, Weak};
 
+#[derive(Debug)]
 struct Node {
     value: u32,
+    parent: RefCell<Weak<Node>>,
     children: RefCell<Vec<Rc<Node>>>,
 }
 
 fn main() {
-    let leaf = Rc::new(Node{
-        value:3,
-        children:RefCell::new(vec![]),
+    let leaf = Rc::new(Node {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
     });
 
-    let branch = Rc::new(Node{
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
+    let branch = Rc::new(Node {
         value: 5,
-        children:RefCell::new(vec![Rc::clone(&leaf)])
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![Rc::clone(&leaf)]),
     });
+
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
 }
