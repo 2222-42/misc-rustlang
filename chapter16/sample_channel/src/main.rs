ccde2d6abecb8a7e28e5600c238028bb4742d5e5
2222-42 <stephen0617@me.com>
2021-01-10 18:27:03 +0900

    feat(chap16): send message multiple times

--- chapter16/sample_channel/src/main.rs
+++ chapter16/sample_channel/src/main.rs
@@ -1,25 +1,37 @@
 use std::sync::mpsc;
 use std::thread;
+use std::time::Duration;
 
 fn main() {
     let (tx, rx) = mpsc::channel();
 
     thread::spawn(move || {
-        let val = String::from("hi");
-        // tx.send(val).unwrap();
-        let result = tx.send(val);
-        match result {
-            Err(error) => {
-                println!("The receiver is dropped: {}", error);
-                // to do something to exit thread.
-            }
-            _ => {
-                println!("Sent.");
-            }
-        };
-        // println!("val is {}", val)
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+        for val in vals {
+            // tx.send(val).unwrap();
+            let result = tx.send(val);
+            match result {
+                Err(error) => {
+                    println!("The receiver is dropped: {}", error);
+                    // to do something to exit thread.
+                }
+                _ => {
+                    println!("Sent.");
+                }
+            };
+            thread::sleep(Duration::from_secs(1));
+            // println!("val is {}", val)}
+        }
     });
 
-    let received = rx.recv().unwrap();
-    println!("Got: {}", received);
+    // let received = rx.recv().unwrap();
+    // println!("Got: {}", received);
+    for received in rx {
+        println!("Got: {}", received);
+    }
 }
