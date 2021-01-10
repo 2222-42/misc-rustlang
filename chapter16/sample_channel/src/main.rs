37f2da35fb16c4c886102539edfd3c2392457320
2222-42 <stephen0617@me.com>
2021-01-10 18:45:29 +0900

    feat(chap16): send message from multiple sender

--- chapter16/sample_channel/src/main.rs
+++ chapter16/sample_channel/src/main.rs
@@ -5,6 +5,8 @@ use std::time::Duration;
 fn main() {
     let (tx, rx) = mpsc::channel();
 
+    let tx1 = mpsc::Sender::clone(&tx);
+
     thread::spawn(move || {
         let vals = vec![
             String::from("hi"),
@@ -26,6 +28,20 @@ fn main() {
             };
             thread::sleep(Duration::from_secs(1));
             // println!("val is {}", val)}
+        }
+    });
+
+    std::thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("message"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
         }
     });
 
