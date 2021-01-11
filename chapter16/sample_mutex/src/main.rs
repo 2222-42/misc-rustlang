c938d6e227aad83df0dac71bd10caf4af4024183
2222-42 <stephen0617@me.com>
2021-01-11 18:40:43 +0900

    feat(chap16): use Arc to share value

--- chapter16/sample_mutex/src/main.rs
+++ chapter16/sample_mutex/src/main.rs
@@ -1,13 +1,13 @@
-use std::rc::Rc;
+use std::sync::Arc;
 use std::sync::Mutex;
 use std::thread;
 
 fn main() {
-    let counter = Rc::new(Mutex::new(0));
+    let counter = Arc::new(Mutex::new(0));
     let mut handles = vec![];
 
     for _ in 0..10 {
-        let counter = Rc::clone(&counter);
+        let counter = Arc::clone(&counter);
         let handle = thread::spawn(move || {
             let mut num = counter.lock().unwrap();
             *num += 1;
