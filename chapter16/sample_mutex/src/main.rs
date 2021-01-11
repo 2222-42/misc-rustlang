241fde7ef908d12955170f34ca423951813761f8
2222-42 <stephen0617@me.com>
2021-01-11 18:37:35 +0900

    chore(chap16): try to share by using Rc<T>

--- chapter16/sample_mutex/src/main.rs
+++ chapter16/sample_mutex/src/main.rs
@@ -1,11 +1,13 @@
+use std::rc::Rc;
 use std::sync::Mutex;
 use std::thread;
 
 fn main() {
-    let counter = Mutex::new(0);
+    let counter = Rc::new(Mutex::new(0));
     let mut handles = vec![];
 
     for _ in 0..10 {
+        let counter = Rc::clone(&counter);
         let handle = thread::spawn(move || {
             let mut num = counter.lock().unwrap();
             *num += 1;
