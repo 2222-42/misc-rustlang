10654daf612fbbe24ee18806bb11a1000101a9e9
2222-42 <stephen0617@me.com>
2021-01-17 08:29:23 +0900

    feat(chap17;blog): not use state_pattern

--- chapter17/blog/src/lib.rs
+++ chapter17/blog/src/lib.rs
@@ -1,126 +1,48 @@
 pub struct Post {
-    state: Option<Box<State>>,
     content: String,
 }
 
 impl Post {
-    pub fn new() -> Post {
-        Post {
-            state: Some(Box::new(Draft {})),
+    pub fn new() -> DraftPost {
+        DraftPost {
             content: String::new(),
         }
     }
 
-    pub fn add_text(&mut self, text: &str) {
-        self.content = self
-            .state
-            .as_ref()
-            .unwrap()
-            .add_text(&self, &text)
-            .to_string()
-    }
-
     pub fn content(&self) -> &str {
-        self.state.as_ref().unwrap().content(&self)
-    }
-
-    pub fn request_review(&mut self) {
-        if let Some(s) = self.state.take() {
-            self.state = Some(s.request_review())
-        }
-    }
-
-    pub fn reject(&mut self) {
-        if let Some(s) = self.state.take() {
-            self.state = Some(s.reject())
-        }
-    }
-
-    pub fn approve(&mut self) {
-        if let Some(s) = self.state.take() {
-            self.state = Some(s.approve())
-        }
+        &self.content
     }
 }
 
-trait State {
-    fn add_text<'a>(&self, post: &'a Post, text: &'a str) -> &'a str;
-    // Not able to borrow mutably and imutably at same time.
-    // fn add_text(&self, post: &mut Post, text: &str);
-    fn request_review(self: Box<Self>) -> Box<State>;
-    fn reject(self: Box<Self>) -> Box<State>;
-    fn approve(self: Box<Self>) -> Box<State>;
-    fn content<'a>(&self, post: &'a Post) -> &'a str {
-        ""
-    }
+pub struct DraftPost {
+    content: String,
 }
 
-struct Draft {}
-
-impl State for Draft {
-    fn add_text<'a>(&self, post: &'a Post, text: &'a str) -> &'a str {
-        text
+impl DraftPost {
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
     }
 
-    fn request_review(self: Box<Self>) -> Box<State> {
-        Box::new(PendingReview { count: 0 })
-    }
-
-    fn reject(self: Box<Self>) -> Box<State> {
-        self
-    }
-
-    fn approve(self: Box<Self>) -> Box<State> {
-        self
-    }
-}
-
-struct PendingReview {
-    count: u32,
-}
-
-impl State for PendingReview {
-    fn add_text<'a>(&self, post: &'a Post, text: &'a str) -> &'a str {
-        &post.content
-    }
-
-    fn request_review(self: Box<Self>) -> Box<State> {
-        self
-    }
-
-    fn reject(self: Box<Self>) -> Box<State> {
-        Box::new(Draft {})
-    }
-
-    fn approve(self: Box<Self>) -> Box<State> {
-        if self.count > 0 {
-            Box::new(Published {})
-        } else {
-            Box::new(PendingReview { count: 1 })
+    pub fn request_review(self) -> PendingReviewPost {
+        PendingReviewPost {
+            content: self.content,
         }
     }
 }
 
-struct Published {}
+pub struct PendingReviewPost {
+    content: String,
+}
 
-impl State for Published {
-    fn add_text<'a>(&self, post: &'a Post, text: &'a str) -> &'a str {
-        &post.content
-    }
-
-    fn request_review(self: Box<Self>) -> Box<State> {
-        self
-    }
-
-    fn reject(self: Box<Self>) -> Box<State> {
-        self
-    }
-
-    fn approve(self: Box<Self>) -> Box<State> {
-        self
-    }
-
-    fn content<'a>(&self, post: &'a Post) -> &'a str {
-        &post.content
+impl PendingReviewPost {
+    pub fn approve(self) -> Post {
+        // if self.count > 0 {
+        //     Box::new(Published {})
+        // } else {
+        //     Box::new(PendingReview { count: 1 })
+        // }
+        Post {
+            content: self.content,
+        }
     }
 }
--- chapter17/blog/src/main.rs
+++ chapter17/blog/src/main.rs
@@ -5,31 +5,30 @@ fn main() {
     let mut post = Post::new();
 
     post.add_text("I ate a salad for lunch today");
-    assert_eq!("", post.content());
 
-    post.request_review();
-    assert_eq!("", post.content());
+    let post = post.request_review();
 
-    post.approve();
-    assert_eq!("", post.content());
+    let post = post.approve();
+    // assert_eq!("", post.content())
+    assert_eq!("I ate a salad for lunch today", post.content());
 
-    post.reject();
-    assert_eq!("", post.content());
+    // post.reject();
+    // assert_eq!("", post.content());
 
-    post.add_text("I ate two salads for lunch today");
-    assert_eq!("", post.content());
+    // post.add_text("I ate two salads for lunch today");
+    // assert_eq!("", post.content());
 
-    post.approve();
-    assert_eq!("", post.content());
+    // post.approve();
+    // assert_eq!("", post.content());
 
-    post.request_review();
-    assert_eq!("", post.content());
+    // post.request_review();
+    // assert_eq!("", post.content());
 
-    post.add_text("I ate three salads for lunch today");
-    assert_eq!("", post.content());
+    // post.add_text("I ate three salads for lunch today");
+    // assert_eq!("", post.content());
 
-    post.approve();
-    assert_eq!("", post.content());
-    post.approve();
-    assert_eq!("I ate two salads for lunch today", post.content());
+    // post.approve();
+    // assert_eq!("", post.content());
+    // post.approve();
+    // assert_eq!("I ate two salads for lunch today", post.content());
 }
