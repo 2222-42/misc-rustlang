3e8ef84c09b97c5b36ad4abb203251629bf3fc19
2222-42 <stephen0617@me.com>
2021-01-20 17:57:38 +0900

    feat(chap18; pattern_with_ref): use ref

--- chapter18/pattern_with_ref/src/main.rs
+++ chapter18/pattern_with_ref/src/main.rs
@@ -2,7 +2,16 @@ fn main() {
     let robot_name = Some(String::from("Bors"));
 
     match robot_name {
-        Some(name) => println!("Found a name: {}", name),
+        Some(ref name) => println!("Found a name: {}", name),
+        None => (),
+    }
+
+    println!("robot_name is {:?}", robot_name);
+
+    let mut robot_name = Some(String::from("Bors"));
+
+    match robot_name {
+        Some(ref mut name) => *name = String::from("Another name"),
         None => (),
     }
 
