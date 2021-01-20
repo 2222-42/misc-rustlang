d339debd642a962f21dd25d2f67ee2167b7939f3
2222-42 <stephen0617@me.com>
2021-01-20 17:10:39 +0900

    chore(chap18): ignore value by _

--- chapter18/ignoring_pattern/src/main.rs
+++ chapter18/ignoring_pattern/src/main.rs
@@ -4,4 +4,25 @@ fn foo(_: i32, y: i32) {
 
 fn main() {
     foo(3, 4);
+
+    let mut setting_value = Some(5);
+    let new_setting_value = Some(10);
+
+    match (setting_value, new_setting_value) {
+        (Some(_), Some(_)) => {
+            println!("Can't overwrite an existing customized value")
+        }
+        _ => {
+            setting_value = new_setting_value;
+        }
+    }
+
+    println!("setting is {:?}", setting_value);
+
+    let numbers = (2, 4, 8, 16, 32);
+    match numbers {
+        (first, _, third, _, fifth) => {
+            println!("Some numbers: {}, {}, {}", first, third, fifth)
+        }
+    }
 }
