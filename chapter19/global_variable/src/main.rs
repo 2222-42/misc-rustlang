97a2786461cc124aa63c2f6f17501819b402b5d2
2222-42 <stephen0617@me.com>
2021-01-25 08:06:30 +0900

    feat(chap19): use mutable global variable

--- chapter19/global_variable/src/main.rs
+++ chapter19/global_variable/src/main.rs
@@ -1,5 +1,17 @@
 static HELLO_WORLD: &str = "Hello, world!";
+static mut COUNTER: u32 = 0;
+
+fn add_to_count(inc: u32) {
+    unsafe {
+        COUNTER += inc;
+    }
+}
 
 fn main() {
     println!("name is : {}", HELLO_WORLD);
+
+    add_to_count(1);
+    unsafe {
+        println!("COUNTER: {}", COUNTER);
+    }
 }
