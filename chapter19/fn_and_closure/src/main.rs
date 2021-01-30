1b9df64ef7919419d07efff60f15d0e569c65def
2222-42 <stephen0617@me.com>
2021-01-30 18:37:32 +0900

    chore(chap19): use whether closure or function pointer

--- chapter19/fn_and_closure/src/main.rs
+++ chapter19/fn_and_closure/src/main.rs
@@ -10,4 +10,10 @@ fn main() {
     let answer = do_twice(add_one, 5);
 
     println!("The answer is {}", answer);
+
+    let list_of_numbers = vec![1,2,3];
+    let list_of_strings1: Vec<String> = list_of_numbers.iter().map(|i| i.to_string()).collect();
+    let list_of_strings2: Vec<String> = list_of_numbers.iter().map(ToString::to_string).collect();
+    println!("The list_of_string is {:?}", list_of_strings1);
+    assert_eq!(list_of_strings1, list_of_strings2);
 }
