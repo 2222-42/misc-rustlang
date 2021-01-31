8d63dabf3f1ee5bc9a69c18fd1690da0eca9abb1
2222-42 <stephen0617@me.com>
2021-02-01 06:35:48 +0900

    feat(chap20): add handle_connection

--- chapter20/hello/src/main.rs
+++ chapter20/hello/src/main.rs
@@ -5,6 +5,12 @@ fn main() {
 
     for stream in listner.incoming() {
         let stream = stream.unwrap();
-        
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0;1024];
+    stream.read(&mut buffer).unwrap();;
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
