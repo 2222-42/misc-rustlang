4732361b31a971a9d998fb8e5febefc6fe72273d
2222-42 <stephen0617@me.com>
2021-02-01 06:57:24 +0900

    refact(chap20): remove duplicate codes

--- chapter20/hello/src/main.rs
+++ chapter20/hello/src/main.rs
@@ -16,26 +16,19 @@ fn handle_connection(mut stream: TcpStream) {
 
     let get = b"GET / HTTP/1.1\r\n";
     
-    if buffer.starts_with(get) {
-        let mut file = File::open("hello.html").unwrap();
-
-        let mut contents = String::new();
-    
-        file.read_to_string(&mut contents).unwrap();
-    
-        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
-        let mut file = File::open("404.html").unwrap();
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
 
-        let mut contents = String::new();
-    
-        file.read_to_string(&mut contents).unwrap();
-    
-        let response = format!("HTTP/1.1 404 NOT FOUND\r\n\r\n{}", contents);
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    }
+    let mut file = File::open(filename).unwrap();
+    let mut contents = String::new();
+
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!("{}{}", status_line,contents);
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 
 }
