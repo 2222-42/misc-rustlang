7305a1a28d969e83112790d065c80a7284836806
2222-42 <stephen0617@me.com>
2021-02-01 06:45:05 +0900

    chore(chap20): add html to response

--- chapter20/hello/src/main.rs
+++ chapter20/hello/src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::prelude::*, net::{TcpListener, TcpStream}};
+use std::{fs::File, io::prelude::*, net::{TcpListener, TcpStream}};
 
 fn main() {
     let listner = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -13,7 +13,14 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0;1024];
     stream.read(&mut buffer).unwrap();;
     // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    let mut file = File::open("hello.html").unwrap();
+
+    let mut contents = String::new();
+
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
