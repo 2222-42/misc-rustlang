394ce1d27b782a30717d6b5ab92750d775bcbc04
2222-42 <stephen0617@me.com>
2021-02-02 06:54:22 +0900

    chore(chap20): add related function `new`

--- chapter20/hello/src/main.rs
+++ chapter20/hello/src/main.rs
@@ -4,7 +4,7 @@ use std::{fs::File, io::prelude::*, net::{TcpListener, TcpStream}, thread, time:
 
 fn main() {
     let listner = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool.new(4);
+    let pool = ThreadPool::new(4);
 
     for stream in listner.incoming() {
         let stream = stream.unwrap();
