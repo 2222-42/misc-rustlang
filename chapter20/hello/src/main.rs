a1a4e7a9be35eb275d1785aa1d25a8859010e7bc
2222-42 <stephen0617@me.com>
2021-02-02 06:40:05 +0900

    chore(chap20): add sleep endpoint

--- chapter20/hello/src/main.rs
+++ chapter20/hello/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::prelude::*, net::{TcpListener, TcpStream}};
+use std::{fs::File, io::prelude::*, net::{TcpListener, TcpStream}, thread, time::Duration};
 
 fn main() {
     let listner = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -15,8 +15,12 @@ fn handle_connection(mut stream: TcpStream) {
     // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
     
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
