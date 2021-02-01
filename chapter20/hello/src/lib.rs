2188e614f040829b04f7bf9683253b3f9d884dd9
2222-42 <stephen0617@me.com>
2021-02-02 07:08:16 +0900

    chore(chap20): add execute with generic type

--- chapter20/hello/src/lib.rs
+++ chapter20/hello/src/lib.rs
@@ -4,4 +4,10 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         ThreadPool
     }
+
+    pub fn execute<F>(&self, f: F) 
+    where F: FnOnce() + Send + 'static
+    {
+        
+    }
 }
