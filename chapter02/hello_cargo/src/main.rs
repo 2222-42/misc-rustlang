10fc65c6a6e1bcd1e0b59d31fe64d02a18cd531c
2222-42 <stephen0617@me.com>
2022-02-11 08:58:02 +0900

    feat(chap02): gcd

--- chapter02/hello_cargo/src/main.rs
+++ chapter02/hello_cargo/src/main.rs
@@ -4,3 +4,22 @@ fn main() {
     let v = vec![0, 1, 2];
     println!("{:?}", v);
 }
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    return n;
+}
+
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14, 15), 1);
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11);
+}
