e237105d0fc12e876cca01fc767a9e9d35643f5d
2222-42 <stephen0617@me.com>
2022-02-11 12:13:41 +0900

    feat(chap2): 引数から読み取る

--- chapter02/hello_cargo/src/main.rs
+++ chapter02/hello_cargo/src/main.rs
@@ -1,8 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::str::FromStr;
 
-    let v = vec![0, 1, 2];
-    println!("{:?}", v);
+fn main() {
+    let mut numbers = Vec::new();
+
+    for arg in env::args().skip(1) {
+        numbers.push(u64::from_str(&arg).expect("error parsing argument"));
+    }
+
+    if numbers.len() == 0 {
+        eprintln!("Usage: gcd NUBMER ...");
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    println!("The greatest common divisor of {:?} is {}", numbers, d);
 }
 
 fn gcd(mut n: u64, mut m: u64) -> u64 {
