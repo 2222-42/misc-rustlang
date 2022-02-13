e274c085eb6541889f17452428e978d9fac84d44
2222-42 <stephen0617@me.com>
2022-02-13 11:23:51 +0900

    feat(chap02): impl escape_time

---
+++ chapter02/mandelbrot/src/main.rs
@@ -0,0 +1,17 @@
+use num::Complex;
+use std::str::FromStr;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+fn escape_tiem(c: Complex<f64>, limit: usize) -> Option<usize> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    for i in 0..limit {
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+        z = z * z + c;
+    }
+    None
+}
