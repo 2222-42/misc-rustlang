7290a6326a31f0f214d966e2d5973776805c093f
2222-42 <stephen0617@me.com>
2022-02-15 18:47:54 +0900

    feat(mandelbrot): impl. parse

--- chapter02/mandelbrot/src/main.rs
+++ chapter02/mandelbrot/src/main.rs
@@ -15,3 +15,43 @@ fn escape_tiem(c: Complex<f64>, limit: usize) -> Option<usize> {
     }
     None
 }
+
+fn parse_pair<T: FromStr>(s: &str, separtor: char) -> Option<(T, T)> {
+    match s.find(separtor) {
+        None => None,
+        Some(index) => match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+            (Ok(l), Ok(r)) => Some((l, r)),
+            _ => None,
+        },
+    }
+}
+
+#[test]
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>("", ','), None);
+    assert_eq!(parse_pair::<i32>("10,", ','), None);
+    assert_eq!(parse_pair::<i32>(",10", ','), None);
+    assert_eq!(parse_pair::<i32>("10,20", ','), Some((10, 20)));
+    assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
+    assert_eq!(parse_pair::<f32>("0.5x", 'x'), None);
+    assert_eq!(parse_pair::<f32>("0.5x1.5", 'x'), Some((0.5, 1.5)));
+}
+
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_pair(s, ',') {
+        Some((re, im)) => Some(Complex { re, im }),
+        None => None,
+    }
+}
+
+#[test]
+fn test_parse_compolex() {
+    assert_eq!(
+        parse_complex("1.25,-0.0625"),
+        Some(Complex {
+            re: 1.25,
+            im: -0.0625
+        })
+    );
+    assert_eq!(parse_complex(",-0.0625"), None)
+}
