80d183f73063f66d282b4f0d736529ddca86aec1
2222-42 <stephen0617@me.com>
2022-02-15 18:48:09 +0900

    fix(*): typo

--- chapter02/mandelbrot/src/main.rs
+++ chapter02/mandelbrot/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn escape_tiem(c: Complex<f64>, limit: usize) -> Option<usize> {
+fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
     let mut z = Complex { re: 0.0, im: 0.0 };
     for i in 0..limit {
         if z.norm_sqr() > 4.0 {
