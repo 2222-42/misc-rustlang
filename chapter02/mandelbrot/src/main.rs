7fca0719337e5c514fed2f3d72fee25a3437906e
2222-42 <stephen0617@me.com>
2022-02-16 06:57:32 +0900

    feat(mandelbrot): renderとwrite_iamge

--- chapter02/mandelbrot/src/main.rs
+++ chapter02/mandelbrot/src/main.rs
@@ -1,5 +1,6 @@
+use image::{png::PNGEncoder, ColorType};
 use num::Complex;
-use std::{process::Command, str::FromStr};
+use std::{fs::File, process::Command, str::FromStr};
 
 fn main() {
     println!("Hello, world!");
@@ -86,4 +87,34 @@ fn test_pixel_to_point() {
             im: -0.75
         }
     )
+}
+
+fn render(
+    pixels: &mut [u8],
+    bounds: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) {
+    assert!(pixels.len() == bounds.0 * bounds.1);
+    for row in 0..bounds.1 {
+        for column in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8,
+            }
+        }
+    }
+}
+
+fn write_image(
+    filename: &str,
+    pixels: &[u8],
+    bounds: (usize, usize),
+) -> Result<(), std::io::Error> {
+    let output = File::create(filename)?;
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Gray(8))?;
+
+    Ok(())
 }
