95c1fecf0c23a7153052c1f691fc7fba354e0ad6
2222-42 <stephen0617@me.com>
2022-02-17 06:27:09 +0900

    feat(mandelbrot): impl. main

--- chapter02/mandelbrot/src/main.rs
+++ chapter02/mandelbrot/src/main.rs
@@ -1,9 +1,44 @@
 use image::{png::PNGEncoder, ColorType};
 use num::Complex;
-use std::{fs::File, process::Command, str::FromStr};
+use std::{fs::File, str::FromStr};
 
 fn main() {
-    println!("Hello, world!");
+    let args = std::env::args().collect::<Vec<_>>();
+    if args.len() != 5 {
+        eprintln!("Usage: {} FILE PIXELS UPPERRIGHT LOWERRIGHT", args[0]);
+        eprintln!(
+            "Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
+            args[0]
+        );
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x').expect("error parsing iamge dimensions");
+    let upper_left = parse_complex(&args[3]).expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4]).expect("error parsing lower right corner point");
+
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    // render(&mut pixels, bounds, upper_left, lower_right);
+    let threads = 8;
+    let rows_per_band = bounds.1 / threads + 1;
+    {
+        let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
+        crossbeam::scope(|spawner| {
+            for (i, band) in bands.into_iter().enumerate() {
+                let top = rows_per_band * i;
+                let height = band.len() / bounds.0;
+                let band_bounds = (bounds.0, height);
+                let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+                let band_lower_right =
+                    pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+                spawner
+                    .spawn(move |_| render(band, band_bounds, band_upper_left, band_lower_right));
+            }
+        })
+        .unwrap();
+    }
+    write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
 }
 
 fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
