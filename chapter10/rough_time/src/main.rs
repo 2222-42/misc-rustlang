de1688e404ae2fb9c75918d529156e4a428303b3
2222-42 <stephen0617@me.com>
2022-03-31 09:38:07 +0900

    chore(chap10): RoughTime
    データを保持する列挙型

---
+++ chapter10/rough_time/src/main.rs
@@ -0,0 +1,40 @@
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+enum TimeUnit {
+    Seconds,
+    Minutes,
+    Hours,
+    Days,
+    Months,
+    Years,
+}
+
+impl TimeUnit {
+    fn plural(self) -> &'static str {
+        match self {
+            TimeUnit::Seconds => "seconds",
+            TimeUnit::Minutes => "minutes",
+            TimeUnit::Hours => "hours",
+            TimeUnit::Days => "days",
+            TimeUnit::Months => "months",
+            TimeUnit::Years => "years",
+        }
+    }
+
+    fn singular(self) -> &'static str {
+        self.plural().trim_end_matches('s')
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+enum RoughTime {
+    InThePast(TimeUnit, u32),
+    JustNow,
+    InTheFuture(TimeUnit, u32),
+}
+
+fn main() {
+    let four_score_and_serven_years_ago = RoughTime::InThePast(TimeUnit::Years, 4 * 20 + 7);
+    let three_hours_from_now = RoughTime::InTheFuture(TimeUnit::Hours, 3);
+    println!("{:?}", four_score_and_serven_years_ago);
+    println!("{:?}", three_hours_from_now);
+}
