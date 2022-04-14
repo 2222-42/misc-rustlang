cb12552f176b3ccf77b0b67f8c18c1a95784bf49
2222-42 <stephen0617@me.com>
2022-04-15 07:47:16 +0900

    chore(chap10): impl add

--- chapter10/binary_tree/src/main.rs
+++ chapter10/binary_tree/src/main.rs
@@ -11,6 +11,27 @@ struct TreeNode<T> {
     right: BinaryTree<T>,
 }
 
+impl<T: Ord> BinaryTree<T> {
+    fn add(&mut self, value: T) {
+        match *self {
+            BinaryTree::Empty => {
+                *self = BinaryTree::NonEmpty(Box::new(TreeNode {
+                    element: value,
+                    left: BinaryTree::Empty,
+                    right: BinaryTree::Empty,
+                }));
+            }
+            BinaryTree::NonEmpty(ref mut node) => {
+                if value <= node.element {
+                    node.left.add(value);
+                } else {
+                    node.right.add(value);
+                }
+            }
+        }
+    }
+}
+
 fn main() {
     use self::BinaryTree::*;
     let jupiter_tree = NonEmpty(Box::new(TreeNode {
@@ -42,6 +63,14 @@ fn main() {
         left: mars_tree,
         right: uranus_tree,
     }));
+
+    println!("{:?}", tree);
+
+    let mut tree = BinaryTree::Empty;
+    tree.add("Mercury");
+    tree.add("Saturn");
+    tree.add("Mars");
+    tree.add("Jupiter");
 
     println!("{:?}", tree);
 }
