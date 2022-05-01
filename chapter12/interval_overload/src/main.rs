25fe43d3b2240463f6b04ea55f2a3d3c6e31b3eb
2222-42 <stephen0617@me.com>
2022-05-01 12:19:41 +0900

    chore(chap12): sort_by_keyを使う
    Ordでないとダメって記述があったけれど、そうでもなかったのはなぜ？

--- chapter12/interval_overload/src/main.rs
+++ chapter12/interval_overload/src/main.rs
@@ -1,4 +1,4 @@
-use std::cmp::Ordering;
+use std::cmp::{Ordering, Reverse};
 
 #[derive(Debug, PartialEq)]
 struct Interval<T> {
@@ -43,4 +43,52 @@ fn main() {
     };
     assert!(!(left < right));
     assert!(!(left >= right));
+
+    let mut intervals = vec![
+        Interval {
+            lower: 10,
+            upper: 20,
+        },
+        Interval { lower: 7, upper: 8 },
+        Interval {
+            lower: 20,
+            upper: 40,
+        },
+    ];
+    println!("{:?}", intervals);
+
+    intervals.sort_by_key(|i| i.upper); // Ordトレイトがないとsort_by_keyできない、
+                                        // という話しだったが、できてしまっており、困っている。
+    assert_eq!(
+        intervals,
+        vec![
+            Interval { lower: 7, upper: 8 },
+            Interval {
+                lower: 10,
+                upper: 20
+            },
+            Interval {
+                lower: 20,
+                upper: 40
+            }
+        ]
+    );
+    println!("{:?}", intervals);
+
+    intervals.sort_by_key(|i| Reverse(i.lower));
+    assert_eq!(
+        intervals,
+        vec![
+            Interval {
+                lower: 20,
+                upper: 40
+            },
+            Interval {
+                lower: 10,
+                upper: 20
+            },
+            Interval { lower: 7, upper: 8 }
+        ]
+    );
+    println!("{:?}", intervals);
 }
