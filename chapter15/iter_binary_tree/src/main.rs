43070f92b9f490c16e2c18da75252ad008da8a17
2222-42 <stephen0617@me.com>
2022-06-20 10:33:22 +0900

    test(chap15): iterationが適切かのテスト

--- chapter15/iter_binary_tree/src/main.rs
+++ chapter15/iter_binary_tree/src/main.rs
@@ -1,44 +1,31 @@
-use iter_binary_tree::binary_tree::{BinaryTree, TreeNode};
+use iter_binary_tree::binary_tree::{BinaryTree};
 
 fn main() {
-    use BinaryTree::*;
-    let jupiter_tree = NonEmpty(Box::new(TreeNode {
-        element: "Jupiter",
-        left: Empty,
-        right: Empty,
-    }));
-
-    let mercury_tree = NonEmpty(Box::new(TreeNode {
-        element: "Mercury",
-        left: Empty,
-        right: Empty,
-    }));
-
-    let mars_tree = NonEmpty(Box::new(TreeNode {
-        element: "Mars",
-        left: jupiter_tree,
-        right: mercury_tree,
-    }));
-
-    let uranus_tree = NonEmpty(Box::new(TreeNode {
-        element: "Uranus",
-        left: Empty,
-        right: Empty,
-    }));
-
-    let tree = NonEmpty(Box::new(TreeNode {
-        element: "Saturn",
-        left: mars_tree,
-        right: uranus_tree,
-    }));
-
-    println!("{:?}", tree);
 
     let mut tree = BinaryTree::Empty;
-    tree.add("Mercury");
-    tree.add("Saturn");
-    tree.add("Mars");
-    tree.add("Jupiter");
+    tree.add("10");
+    tree.add("04");
+    tree.add("08");
+    tree.add("03");
+    tree.add("07");
+    tree.add("09");
+    tree.add("05");
+    tree.add("06");
+    tree.add("13");
+    tree.add("11");
+    tree.add("14");
 
     println!("{:?}", tree);
+
+    let mut v = Vec::new();
+    for x in &tree {
+        v.push(*x);
+    }
+    assert_eq!(v, vec!["03", "04","05", "06", "07", "08", "09", "10", "11", "13", "14"]);
+
+    let added_prefix = tree.iter().map(|x| format!("number-{}", x)).collect::<Vec<_>>();
+    assert_eq!(
+        added_prefix,
+        vec!["number-03", "number-04", "number-05", "number-06", "number-07", "number-08", "number-09", "number-10", "number-11", "number-13", "number-14"]
+    )
 }
